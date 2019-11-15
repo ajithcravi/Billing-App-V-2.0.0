5c9aea0dc754d143c3c70761ca44dafa95023a84
ajithcravi <craviajith@gmail.com>
2019-11-15 18:27:02 +0530

    static files served

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const express = require ("express")
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 2000;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(
+    `Stop staring @ me and start coding... \nI am working fine in port ${port} `
+  );
+});
