3582534d7b0d08657cdfb36fd39cc88a0df33fa0
ajithcravi <craviajith@gmail.com>
2019-11-18 10:50:43 +0530

    mongoose connection established

---
+++ Models/form.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const formSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  contactNo: {
+    type: Number,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  }
+});
+
+const FormDetails = mongoose.model("test", formSchema);
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const router = require("./Routes/index");
 const port = process.env.PORT || 3000;
 
-app.use("/", router);
+mongoose.connect("mongodb://localhost/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+let db = mongoose.connection;
+db.then(() => console.log("db connected succesfully"));
+db.catch(error => console.log("error"));
+
+app.use("/", require("./Routes/index"));
 
 app.use("/secrets", require("./Routes/secrets"));
 app.use("/public", require("./Routes/public"));
