f13e459c373eab2c58e00bad5acb3ff129eb2877
ajithcravi <craviajith@gmail.com>
2019-11-19 18:14:52 +0530

    if loop created for product quantity below 0

--- Routes/billing.js
+++ Routes/billing.js
@@ -11,21 +11,39 @@ router.post("/", (req, res) => {
     productCategory: req.body.productCategory,
     productQuantity: req.body.productQuantity
   };
+  if (data.productQuantity < 1) {
+    console.log(`Please enter a value ablove 0`);
+    res.redirect("bill.html");
+    return;
+  }
   console.log(data);
   ProductModel.findOne({
     itemName: data.productName,
     category: data.productCategory
   })
     .then(doc => {
-      ProductModel.findByIdAndUpdate(
-        doc._id,
-        { qty: doc.qty - data.productQuantity },
-        { new: true }
-      )
-        .then(doc => console.log(`Succesfully updated\n ${doc}`))
+      ProductModel.findById({ _id: doc._id })
+        .then(doc => {
+          if (doc.qty < data.productQuantity) {
+            console.log(`Sorry! only ${doc.qty} items available`);
+          } else {
+            let price = doc.rate * data.productQuantity;
+            doc.qty -= data.productQuantity;
+            console.log(price);
+            console.log(doc);
+            ProductModel.findByIdAndUpdate(
+              { _id: doc._id },
+              { qty: doc.qty },
+              { new: true, useFindAndModify: false }
+            )
+              .then(doc => console.log(doc))
+              .catch(err => console.log(err));
+          }
+        })
         .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
+  res.redirect("bill.html");
 });
 
 module.exports = router;
